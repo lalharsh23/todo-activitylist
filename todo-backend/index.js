08723d84f9e186600dd0581b980ecfab370b6eb9
Harsh lal <143641863+lalharsh23@users.noreply.github.com>
2024-06-20 02:00:29 +0530

    Update index.js

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -15,7 +15,13 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    method:["POST","GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
