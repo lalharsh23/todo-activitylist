5ba6a2a70b9127ab9e97de076e78b368a0995ad9
Harsh lal <143641863+lalharsh23@users.noreply.github.com>
2024-06-20 12:15:56 +0530

    Updated mongoose connection

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -7,21 +7,36 @@ const activityRoutes = require("./routes/activity");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(
-    "mongodb+srv://lalharsh1:Harshlal23@cluster0.sk2xr1e.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+const DB =
+    "mongodb+srv://lalharsh1:HekwAbKP47EsliIt@cluster0.sk2xr1e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/activity-project";
 
-app.use(cors(
-  {
-    origin:["https://deploy-mern-1whq.vercel.app"],
-    method:["POST","GET"],
-    credentials: true
-  }
-));
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//acquire the connection(to check if it's successful)
+const db = mongoose.connection;
+
+//error
+db.on("error", function (err) {
+    console.log(err.message);
+});
+
+//up and running then print the message
+db.once("open", function () {
+    console.log(
+        `Successfully connected to the database :: MongoDB`
+    );
+});
+
+app.use(
+    cors({
+        origin: "*",
+        method: ["POST", "GET"],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
