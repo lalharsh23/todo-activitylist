128825df9e2990bf59d9f64701c10d8416b13366
Harsh lal <143641863+lalharsh23@users.noreply.github.com>
2024-06-20 12:34:36 +0530

    Update backend url

--- todo-frontend/src/components/Signup.js
+++ todo-frontend/src/components/Signup.js
@@ -10,7 +10,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/users/signup', { username, password });
+            const res = await axios.post('https://todo-activitylist.onrender.com/api/users/signup', { username, password });
             localStorage.setItem('token', res.data.token);
             navigate('/todos');
         } catch (err) {
