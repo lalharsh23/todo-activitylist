0431884760176d4ffbc1ba56722a83ec9e652291
Harsh lal <143641863+lalharsh23@users.noreply.github.com>
2024-06-20 12:34:01 +0530

    Update backend url

--- todo-frontend/src/components/Login.js
+++ todo-frontend/src/components/Login.js
@@ -10,7 +10,7 @@ export default function Example() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", {
+      const res = await axios.post("https://todo-activitylist.onrender.com/api/users/login", {
         username,
         password,
       });
